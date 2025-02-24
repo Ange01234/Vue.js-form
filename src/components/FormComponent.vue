c788fb0e6d7513e94f3939853c4520a00de0c7d0
Ange01234 <cokou-ange.hounnouvi@epitech.eu>
2025-02-24 14:28:46 +0100

    {feat}: update

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="flex flex-col md:flex-row h-dvh bg-cover md:bg-center bg-left" style="background-image: url('/gradient.svg');">
     <!-- Partie gauche (image) -->
-    <div class="w-full md:w-1/2 flex items-center justify-center relative py-10 h-full">
-      <div class="absolute w-3/4 h-3/4"></div>
+    <div class="w-full md:w-1/2 flex items-center justify-center relative md:py-10 h-2/5 md:h-full">
       <img src="/character.svg" alt="Illustration" class="relative w-2/3 md:w-1/2" />
     </div>
 
     <!-- Partie droite (formulaire) -->
-    <div class="w-full md:w-1/2 backdrop-blur-lg shadow-lg flex justify-center py-10 h-full overflow-auto">
-      <div class="p-5 md:p-5 w-11/12 md:w-3/4 max-h-80">
-        <h1 class="text-2xl md:text-3xl font-medium mb-6 text-black text-center md:text-left">Yellow ! Rejoignez l’aventure !</h1>
-        <p v-if="step === 1 || step === 2" class="text-gray-600 mb-6 text-lg md:text-xl text-center md:text-left">Identifiez-vous</p>
+    <div class="w-full md:w-1/2 bg-yellow-100/30 backdrop-blur-lg md:shadow-lg flex justify-center md:py-10 h-3/5 md:h-full">
+      <div class="p-0 md:p-5 w-11/12 md:w-3/4 max-h-80">
+        <h1 class="text-2xl md:text-3xl font-medium mt-2 mb-2 md:mb-6 text-black text-center md:text-left">Yellow ! Rejoignez l’aventure !</h1>
+        <p v-if="step === 1 || step === 2" class="text-gray-600 md:mb-6 mb-2 text-lg md:text-xl text-center md:text-left">Identifiez-vous</p>
         <p v-if="step === 3" class="text-gray-600 mb-6 text-lg md:text-xl text-center md:text-left">Plus qu'une étape</p>
 
         <hr class="mb-5">
@@ -18,17 +17,17 @@
         <form @submit.prevent="submitForm">
           <!-- Étape 1 -->
           <div v-if="step === 1">
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Nom *</label>
               <input required="true" type="text" v-model="form.nom" placeholder="LABELLE" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none " />
               <p v-if="error.nom" class="text-red-500 text-center">{{ error.nom }}</p>
             </div>
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Prénom *</label>
               <input required type="text" v-model="form.prenom" placeholder="MAYA" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none" />
               <p v-if="error.prenom" class="text-red-500 text-center">{{ error.prenom }}</p>
             </div>
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Date de naissance *</label>
               <input required type="date" v-model="form.date" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none" />
               <p v-if="error.date" class="text-red-500 text-center">{{ error.date }}</p>
@@ -37,12 +36,12 @@
 
           <!-- Étape 2 -->
           <div v-if="step === 2">
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Numéro de téléphone *</label>
               <input required type="text" v-model="form.numero" @focus="formatNumero" @input="formatNumero" placeholder="01 XXXXXXXX" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none" />
               <p v-if="error.number" class="text-red-500 text-center">{{ error.number }}</p>
             </div>
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Ville *</label>
               <input required type="text" v-model="form.town" @input="filterTowns" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none" />
               <ul v-if="Towns.length">
@@ -52,7 +51,7 @@
               </ul>
               <p v-if="error.town" class="text-red-500 text-center">{{ error.town }}</p>
             </div>
-            <div class="mb-5 md:mb-8">
+            <div class="mb-3 md:mb-8">
               <label class="block text-gray-700">Quartier *</label>
               <input required type="text" v-model="form.district" class="w-full px-4 py-3 md:py-4 border rounded-lg focus:outline-none" />
               <!-- <ul v-if="Districts.length">
@@ -84,7 +83,7 @@
           </div>
 
           <!-- Boutons -->
-          <div class="flex flex-col mb-3">
+          <div class="flex flex-col mb-5">
             <button type="button" @click="nextStep" v-if="step < 3" class="bg-yellow-500 text-white py-3 md:py-4 px-4 rounded-lg w-full">Continuer</button>
             <button type="submit" v-if="step === 3" class="bg-yellow-500 text-white py-3 md:py-4 px-4 rounded-lg w-full">Vous êtes prêt !</button>
           </div>
@@ -152,6 +151,8 @@ export default {
 
         if (!this.form.date) {
           this.error.date = "La date de naissance est requise.";
+        } else if (this.calculateAge(this.form.date) < 18) {
+          this.error.date = "Vous devez avoir au moins 18 ans pour vous inscrire.";
         }
       } else if (this.step === 2) {
         if (!this.form.numero || this.form.numero.length !== 10) {
